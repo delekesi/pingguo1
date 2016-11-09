56807c3fe1db9cd5e08c4f65d05ffc3665453ccb
delekesi <673745069@qq.com>
2016-11-09 19:18:21 +0800

    pingguos
    pingguo

--- js/index.js
+++ js/index.js
@@ -1,234 +1,127 @@
 $(function(){
-    var $logo1=$(".xx");
-    var $li=$(".sousuo1");
-    var $logo=$(".ull li:not(.pingguo)");
-    var $cang=$(".cang");
-    var $input=$(".cang input");
-    var $hh=$(".cang h3");
-    var $zz=$(".cang ul");
-    var $dakai=$(".dakai");
-    var $xian=$(".xian");
-    var $xian1=$(".xian1");
-    var $leb=$(".leb");
-    var $nn=window.innerHeight - 44;
-    var $mm=window.innerWidth;
-
-
-   $leb.css({height:$nn + "px"})
-
-
-
-
-    $li.click(function(){
-     $logo.css({
-         transition: "all .5s",
-         transform:"scale(0,0)",
-
-
-     })
-        $logo1.css({
-            transition: "all .5s",
-            transform:"scale(1,1)",
+    var close=$('.close');
+    var open=$('.searcher');
+    // 大屏
+    $(open).click(function(){
+        $('.search-box').css({
+            display:'block'
         })
-        $cang.css({
-            transition: "all .5s",
-            transform:"scale(1,1)",
+        setTimeout(function(){
+            $('.sy a:nth-child(1)').addClass('hot')
+            $('.sy a:nth-child(2)').addClass('hot')
+            $('.sy a:nth-child(3)').addClass('hot')
+            $('.sy a:nth-child(4)').addClass('hot')
         })
-        $input.css({
-            transition: "all 1s",
-            transform:"scale(1,1)",
+        $('.nav li').not($('.logo')).css({
+            display:'none'
         })
-        $hh.css({
-            transition: "all 1.2s",
-            transform:"scale(1,1)",
+        $(close).css({
+            display:'block'
         })
-        $('.bottom a:nth-child(1)').css({
-            transition: "all 1.2s",
-            transform:"scale(1,1)",
+        $(document.body).css({
+            background:'#eee'
         })
-        $('.bottom a:nth-child(2)').css({
-            transition: "all 1.6s",
-            transform:"scale(1,1)",
-        })
-        $('.bottom a:nth-child(3)').css({
-            transition: "all 2s",
-            transform:"scale(1,1)",
-        })
-        $('.bottom a:nth-child(4)').css({
-            transition: "all 2.4s",
-            transform:"scale(1,1)",
-        })
-
-
-
-
     })
-    $logo1.click(function(){
-     $logo.css({
-         transition: "all .5s",
-         transform:"scale(1,1)",
-     }),
-        $cang.fadeIn("fast",function(){
-            $cang.css({
-                transition: "all .5s",
-                transform:"scale(0,0)",
-            })
-        }),
-        $logo.css({
-            transition: "all .5s",
-            transform:"scale(1,1)",
-        }),
-         $logo1.css({
-             transition: "all .5s",
-             transform:"scale(0,0)",
-         })
+    $(close).click(function(){
+        $('.search-box').css({
+            display:'none'
+        })
+        $('.nav li').css({
+            display:'inline-block'
+        })
+        $(this).css({
+            display:'none'
+        })
+        $('.sy a:nth-child(1)').removeClass('hot')
+        $('.sy a:nth-child(2)').removeClass('hot')
+        $('.sy a:nth-child(3)').removeClass('hot')
+        $('.sy a:nth-child(4)').removeClass('hot')
     })
 
-    var num=0;
-    $dakai.click(function(){
-        
-        
+    // 小屏
+    $('.mini').click(function(){
+        var ht=$(window).height();
+        $(".mini span:nth-child(1)").toggleClass('active')
+        $(".mini span:nth-child(2)").toggleClass('active1')
+        $('.min-nav .goshop').toggleClass('nav-syn')
+        setTimeout(function(){
+            var h=$(window).height();
 
-        if(num==0){
-
-            num=1;  
-            $xian.css({
-            marginTop:"-5px",
-            transform:"rotate(135deg)"
-
-        })
-        $xian1.css({
-            marginTop:"0",
-            transform:"rotate(45deg)"
-
-        })
-        $leb.slideToggle();
-        $('.leb1 li:nth-child(1)').css({
-            transition: "all .5s",
-            transform:"scale(1,1)",
-        })
-        $('.leb1 li:nth-child(2)').css({
-            transition: "all .7s",
-            transform:"scale(1,1)"
-        })
-        $('.leb1 li:nth-child(3)').css({
-            transition: "all .9s",
-            transform:"scale(1,1)",
-        })
-        $('.leb1 li:nth-child(4)').css({
-            transition: "all 1.1s",
-            transform:"scale(1,1)"
-        })
-        $('.leb1 li:nth-child(5)').css({
-            transition: "all 1.3s",
-            transform:"scale(1,1)",
-        })
-        $('.leb1 li:nth-child(6)').css({
-            transition: "all 1.5s",
-            transform:"scale(1,1)"
-        })
-        $('.leb1 li:nth-child(7)').css({
-            transition: "all 1.7s",
-            transform:"scale(1,1)"
-        })
-         
-     
-        }else if(num==1){
-          
-            
-                num=0;
-            $xian.css({
-                marginTop: "-10px",
-                transform: "rotate(0deg)"
-
+            $('.nav-sy').toggleClass('nav-syh')
+            $('.nav-sy').height(h)
+            $('body').height(h).css('overflow','hiddenx')
+            setTimeout(function(){
+                $('.nav-sy li:nth-child(1)').toggleClass('hot')
+                $('.nav-sy li:nth-child(2)').toggleClass('hot')
+                $('.nav-sy li:nth-child(3)').toggleClass('hot')
+                $('.nav-sy li:nth-child(4)').toggleClass('hot')
+                $('.nav-sy li:nth-child(5)').toggleClass('hot')
+                $('.nav-sy li:nth-child(6)').toggleClass('hot')
+                $('.nav-sy li:nth-child(7)').toggleClass('hot')
             })
-            $xian1.css({
-                marginTop: "10px",
-                transform: "rotate(0deg)"
-
-            })
-            $leb.slideToggle();
-            $('.leb1 li:nth-child(1)').css({
-                transition: "all 1.7s",
-                transform: "scale(0,0)",
-            })
-            $('.leb1 li:nth-child(2)').css({
-                transition: "all 1.5s",
-                transform: "scale(0,0)"
-            })
-            $('.leb1 li:nth-child(3)').css({
-                transition: "all 1.3s",
-                transform: "scale(0,0)",
-            })
-            $('.leb1 li:nth-child(4)').css({
-                transition: "all 1.1s",
-                transform: "scale(0,0)"
-            })
-            $('.leb1 li:nth-child(5)').css({
-                transition: "all .9s",
-                transform: "scale(0,0)",
-            })
-            $('.leb1 li:nth-child(6)').css({
-                transition: "all .7s",
-                transform: "scale(0,0)"
-            })
-            $('.leb1 li:nth-child(7)').css({
-                transition: "all .5s",
-                transform: "scale(0,0)"
-            })
-           
-         
-        
-
-        }
-
-        
-
-       
         })
+    })
 
 
-  
-      
-       var $hh=$('.hezi1 ul')
-       var $hs=$(".hezi1 h3")
-       
-       $hs.click(function(){
-        $hh.eq($hs.index(this)).slideToggle();
-       })
 
-       
-       
+    //banner
+    var tu=$('.banner-box li');
+    var btns=$('.btns a');
 
 
 
 
+    // footer
+    var tit=$('.tit');
+    var xx=$('.xx1');
+   tit.click(function(){
+        var i=tit.index(this);
+       xx.eq(i).toggleClass('has');
+
+    })
 
 
-       var $imgs=$('.banner2');
-
-       var $img=$(".ban");
-       var num=0;
-
-       t=setInterval(move,2000)
-
-       function move(){
-           $imgs.animate({marginLeft:'-$mm'},1000,function(){
-               $imgs.css('marginLeft','0');
-               $('.banner2 .ban:first').remove().clone(true).appendTo($imgs);
-           })
-       }
-       $('.banner').hover(function(){
-           clearInterval(t)
-       },function(){
-           t=setInterval(move,2000)
-       })
+   // banner
+   $(".btns div").mouseover(function(event){
+			var index=$(this).index(); //获取当前的索引值
+			i=index;
+			$(".banner-box ul li").eq(index).show().siblings().hide();
+			$(this).addClass('btnns').siblings().removeClass('btnns');
+		})
 
 
-       
-})
-       
+	var i=0;
+		var t=setInterval(move,2000);
+		function move(){
+			i++;
+			if(i==5){
+				i=0;
+			}
+			$(".btns div").eq(i).addClass("btnns").siblings().removeClass();
+			$(".banner-box ul li").eq(i).fadeIn().siblings().fadeOut();
+		}
 
+		function move1(){
+			i--;
+			if(i==-1){
+				i=4;
+			}
+			$(".btns div").eq(i).addClass("btnns").siblings().removeClass();
+			$(".banner-box ul li").eq(i).fadeIn().siblings().fadeOut();
+		}
 
+		$(".banner-box").hover(function(){
+			clearInterval(t);
+		},function(){
+			t=setInterval(move,2000);
+		})
 
-   
+		//左右点击
+		$(".rightss").click(function(){
+			move();
+		})
+		$(".leftss").click(function(){
+			move1();
+		})
+
+	})
